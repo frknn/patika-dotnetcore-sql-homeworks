5649cd528ab3f76cf9474b92f2719a763457c4c2
Furkan SETBAŞI <43930639+frknn@users.noreply.github.com>
2021-08-07 18:24:30 +0300

    Create odev_10.sql

---
+++ odev_10.sql
@@ -0,0 +1,11 @@
+-- 1. Soru
+SELECT city,country FROM city
+LEFT JOIN country ON city.country_id = country.country_id;
+
+-- 2. Soru
+SELECT payment_id, first_name, last_name FROM customer
+RIGHT JOIN payment ON customer.customer_id = payment.customer_id;
+
+-- 3. Soru
+SELECT rental_id, first_name, last_name FROM customer
+FULL JOIN rental ON customer.customer_id = rental.customer_id;
